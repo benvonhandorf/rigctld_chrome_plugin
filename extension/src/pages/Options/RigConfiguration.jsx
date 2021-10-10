1040fa1a5f671aa46a603aac7ad391e6fc6d28f1
Ben Von Handorf <ben@skyironstudio.com>
2021-10-10 06:57:56 -0400

    Mui components rendering on RigEntry

--- extension/src/pages/Options/RigConfiguration.jsx
+++ extension/src/pages/Options/RigConfiguration.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import RigEntry from './RigEntry';
 
 const RigConfiguration = (props) => {
-    let rig_configuration = props.rig_configuration;
-    let rig_control_settings = props.rig_control_settings;
+    let rig_configuration = props.rig_configuration || [];
+    let rig_control_settings = props.rig_control_settings || [];
 
     return (
         <div className="rig_list">
@@ -11,6 +12,7 @@ const RigConfiguration = (props) => {
                 <span className="name">{rig.name}</span>
             )
         }
+        <RigEntry />
         </div>
     );
 };
--- extension/src/pages/Options/RigEntry.jsx
+++ extension/src/pages/Options/RigEntry.jsx
@@ -1,8 +1,42 @@
 import React from 'react';
+import Box from '@mui/material/Box';
+import {TextField, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 const RigEntry = (props) => {
     return (
+        <div>
+            <h1>Rig Configuration</h1>
+            <TextField
+                required
+                id="rig-name"
+                label="Name"
+            />
 
+            <h2>Connection Settings</h2>
+            <FormControl>
+                <InputLabel id="rig-type-label">Connection Type</InputLabel>
+                <Select
+                    labelId="rig-type-label"
+                    id="rig-type"
+                    label="Type"
+                >
+                    <MenuItem value="rigctld">Rigctld</MenuItem>
+                    <MenuItem value="gqrx">Gqrx or legacy Rigctld</MenuItem>
+                </Select>
+            </FormControl>
+
+            <TextField
+                required
+                id="rig-connection-host"
+                label="Host"
+            />
+            <TextField
+                required
+                id="rig-connection-port"
+                label="Port"
+            />
+
+        </div>
     );
 };
 
